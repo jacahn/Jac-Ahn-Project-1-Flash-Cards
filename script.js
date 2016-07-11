ce387b869bd62d8ee68743cfd22383208767c519
Jacqui Ahn <jsahn23@gmail.com>
2016-07-11 15:49:21 -0400

    adds clickCounter to cycle thru array indexCards (displays every ques and answer)

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   console.log("I'm ready")
 
+  var clickCounter = 0;
 
   $(function(){
     $("#card-1").flip({
@@ -21,36 +22,80 @@ $(document).ready(function(){
 
   //click event: class of button clicked, function called
   $("button").on("click", function(){
-    //instantiate var, grabs value of input
-    //.val generally only on input fields
-//    var input = $("input").val();
-    //changes html of h1
-    $(".other-front").html(quesOne.ques);
-    $(".other-back").html(quesOne.ans);
+    //changes html of front and back of card
+    $(".other-front").html(indexCards[clickCounter % indexCards.length].ques);
+    $(".other-back").html(indexCards[clickCounter % indexCards.length].ans);
+    clickCounter++;
   })
 
+  //change html of front and back of card with random choice of question among the array
 
-  //Returns a random number between 0-9
-  function getRandomInt(min, max) {
-    return Math.floor(Math.random() * quesArray.length);
+//  var quesArray=[quesOne, quesTwo];
+  //Returns a random number between 0-quesArray.length
+  var randomNum = function getRandomInt() {
+    return Math.floor(Math.random() * indexCards.length);
+    console.log("function getRandomInt is working")
   }
 
 
-  var quesOne = {
-    ques: "What are two ways to create an object?",
-    ans: "Object literal notation and object constructor"
+  if(randomNum===0){
+    $("button").on("click", function(){
+      //changes html of front and back of card
+      $(".other-front").html(quesOne.ques);
+      $(".other-back").html(quesOne.ans);
+      getRandomInt();
+      console.log("Ques 1")
+    })
+  } else if (randomNum ===1){
+    $("button").on("click", function(){
+      //changes html of front and back of card
+      $(".other-front").html(quesTwo.ques);
+      $(".other-back").html(quesTwo.ans);
+      getRandomInt();
+      console.log("Ques 2")
+    })
   }
+
+
+
+  var indexCards = [
+    {
+      ques: "Question: What are two ways to create an object?",
+      ans: "Answer: 1. Object literal notation 2. Object constructor notation"
+    },
+
+    {
+      ques: "Question: What is a method?",
+      ans: "Answer: A function that is attached to an object"
+    },
+
+
+    {
+      ques: "Question: How do you access 'Mary' in myArray =['Bob', 'Mary', 'Kim']",
+      ans: "Answer: myArray[1]"
+    },
+
+
+    {
+      ques: "Question: what does .val() method do?",
+      ans: "Answer: It's used to get values of form elements like 'input', 'select', and 'textarea'."
+    },
+
+
+    {
+      ques: "Question: What is a callback?",
+      ans: "Answer: A piece of executable code that is passed as an argument to other code, which is expected to invoke that code at some convenient time."
+    },
+
+
+  ]
+
+  indexCards[0]
+
 /*
-  var quesTwo = {
-    'question': "In the following array, how do we access 'Mary'? \n"
-      "\t var names =["Bill", "Mary", "Ken"]"
-
-    'answer: "names[1];"'
-  }
-
   var quesThree = {
-    'question: "What does a modulus return"'
-    'answer: "The remainder of a division operation"'
+    ques: "What does a modulus return?"
+    answer: "The remainder of a division operation"
   }
   var  = {
 
@@ -69,7 +114,7 @@ $(document).ready(function(){
 
   }
 
-  var quesArray=[quesOne, quesTwo, quesThree]
+
 */
 
   });
