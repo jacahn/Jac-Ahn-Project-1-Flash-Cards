d1dc32bb0edad3b3a1c66e067b18e0f673c22d74
Jacqui Ahn <jsahn23@gmail.com>
2016-07-13 09:50:28 -0400

    adds checkboxes to cards

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
   $(function(){
     $("#card-1").flip({
       axis: "y", // y or x
-      reverse: false, // true and false
+      reverse: true, // true and false
       trigger: "click", // click or hover
       speed: '400',
       front: $('.other-front'),
@@ -21,10 +21,16 @@ $(document).ready(function(){
 //  });
 
   //click event: class of button clicked, function called
+  var checkboxy = $("<input type='checkbox' id='checkboxy'></input>")
   $("button").on("click", function(){
     //changes html of front and back of card
+
+
+
+
     $(".other-front").html(indexCards[clickCounter % indexCards.length].ques);
     $(".other-back").html(indexCards[clickCounter % indexCards.length].ans);
+    $('.other-front').prepend('<input type="checkbox" name="myCheckbox" />')
     clickCounter++;
   })
 
@@ -60,43 +66,69 @@ $(document).ready(function(){
 
   var indexCards = [
     {
-      ques: "Question: What are two ways to create an object?",
-      ans: "Answer: 1. Object literal notation 2. Object constructor notation"
+
+      ques: "Question 1: What are two ways to create an object?",
+      ans: "Answer: 1. Object literal notation 2. Object constructor notation",
+      show: true
     },
 
     {
-      ques: "Question: What is a method?",
-      ans: "Answer: A function that is attached to an object"
-    },
-
-
-    {
-      ques: "Question: How do you access 'Mary' in myArray =['Bob', 'Mary', 'Kim']",
-      ans: "Answer: myArray[1]"
+      ques: "Question 2: What is a method?",
+      ans: "Answer: A function that is attached to an object",
+      show: true
     },
 
 
     {
-      ques: "Question: what does .val( ) method do?",
-      ans: "Answer: It's used to get values of form elements like 'input', 'select', and 'textarea'."
+      ques: "Question 3: How do you access 'Mary' in myArray =['Bob', 'Mary', 'Kim']",
+      ans: "Answer: myArray[1]",
+      show: true
     },
 
 
     {
-      ques: "Question: What is a callback?",
-      ans: "Answer: A piece of executable code that is passed as an argument to other code, which is expected to invoke that code at some convenient time."
+      ques: "Question 4: what does .val( ) method do?",
+      ans: "Answer: It's used to get values of form elements like 'input', 'select', and 'textarea'.",
+      show: true
     },
 
 
+    {
+      ques: "Question 5: What is a callback?",
+      ans: "Answer: A piece of executable code that is passed as an argument to other code, which is expected to invoke that code at some convenient time.",
+      show: true
+    },
+
+    {
+      ques: "Question 6: What's the difference between <strong>button.on('click', handleClickEvent );</strong> <br>and <br><strong>button.on('click', handleClickEvent( ) );</strong>",
+      ans: "Answer: The latter handleClickEvent( ) invokes the function, the first handleClickEvent references it.",
+      show: true
+    },
+
+    {
+      ques: "Question 7: What does a modulus return?",
+      ans: "Answer: The remainder of a division operation",
+      show: true
+    }
   ]
 
   indexCards[0]
-
 /*
-  var quesThree = {
-    ques: "What does a modulus return?"
-    answer: "The remainder of a division operation"
-  }
+  checkboxy.click(function(){
+      if (this.checked) {
+        var checkedArray = new Array();
+
+//        $("input:checkbox[name=type]:checked").each(function() {
+          $("checkboxy:checked").each(function(){
+          checkedArray.push($(this).val());
+        });
+        alternateCards(selected);
+        console.log("checked")
+      }
+  })
+*/
+/*
+
   var  = {
 
   }
